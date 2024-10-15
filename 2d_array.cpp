d849fa0fcb179dcde626b1869a4a491cfe59f2af
nexusameer <ameerk10fw@gmail.com>
2024-10-15 14:09:57 +0500

    display

--- 2d_array.cpp
+++ 2d_array.cpp
@@ -39,6 +39,7 @@ int main(){
             
         }
     }
+    cout<<"name  age  address  midterm  quiz1  quiz2  quiz3 "<<endl;
     for(int i=0; i<5; i++){
         for(int j=0; j<7; j++){
             cout<<arr[i][j]<<" ";
